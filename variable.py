a6b41f4ecf6faac94f74bb1e841c2b1da5d1e8f6
Hitherto-crypto <vmoses616@gmail.com>
2021-03-11 16:12:09 +0300

    Hands on python

---
+++ variable.py
@@ -0,0 +1,29 @@
+sum=(2+2) #sum of numbers
+print(sum)
+
+Product=(5*4) #product of numbers
+print(Product)
+
+Division=(20/4) #division of values
+print(Division)
+
+Division=(20//6) #floor division of values does not include the remainder
+print(Division)
+
+sqr=(5**2) #squares the numbers
+print(sqr)
+
+pwr=(5**4) #shows 5 to the powers of 4
+print(pwr)
+
+Width=(20)
+Height=(5*10)
+print(Width*Height) #shows thebproduct of the width against the height
+
+res=(4*3.75-1)
+print(res) #operators with mixed type operand to floating point.
+
+tax=(12.5/100)
+print(tax)
+price=(100.50)
+print(price*tax) #last printed to a variable.
