6381ae5436b0012a8714586286e2e2e754729f67
Hitherto-crypto <78306859+Hitherto-crypto@users.noreply.github.com>
2021-03-11 17:26:49 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,51 @@
+'personal cars' #single quotes
+print('personal cars')
+'doesn\'t' #use \' to escape the single qoute...
+print('doesn\'t')
+'"Yes," they said.'
+print('"Yes," they said.')
+"\"Yes,\" they said."
+print("\"Yes,\" they said.")
+
+s='First line.\nSecond line.' #\n means newline
+s #without print(), \n is included in the output
+s
+print(s)
+
+print ('C:\some\name') #here \n means newline
+print(r'C:\some\name') #note the r before the quote
+
+'py' 'thon'
+text=('put several strings within parenthesis''to have them joined together')
+print(text)
+prefix='py' #can't concatenate a variable an a string literal
+
+print(prefix+'thon')
+
+word='python'
+word[0] # character in position 0
+print(word[0])
+word[5] # charcter in position 5
+print(word[5])
+word[-1] # last character
+print(word[-1])
+word[-6] #second-last character
+print(word[-6])
+
+word[0:2] # characters from position 0 (included) to 2 (excluded)
+print(word[0:2])
+word[2:5] # characters from position 2 (included) to 5 (excluded)
+print(word[2:5])
+word[:2] # character from the beginning to position 2 (excluded)
+print(word[:2])
+word[4:] # characters from position 4 (included) to the end
+print(word[4:])
+
+s = 'supercalifragilisticexpialidocious'
+len(s)
+print(len(s))
+
+
+
+
+
